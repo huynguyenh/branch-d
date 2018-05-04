4a485bb1c5991c9d6878507b0ad50d13f5bbece6
huynguyenh <hoanghuy2908@gmail.com>
2018-05-05 00:14:01 +0700

    delete list of branch

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,46 @@ fn main() {
         .about("Do some stupid stuff noone care to do")
         .get_matches();
 
-    let repo_name = get_git_hub_repo();
+    let list_of_branches = get_list_of_branches();
 
-    if repo_name != "" {
-        println!("{}", repo_name);
+    if list_of_branches.len() > 1 {
+        for i in list_of_branches {
+            if i.len() > 0 {
+                delete_branch(&i)
+            }
+        }
     }
 }
 
-fn get_git_hub_repo() -> String {
+fn delete_branch(name: &String) {
     let output = Command::new("git")
-        .arg("config")
-        .arg("--get")
-        .arg("remote.origin.url")
+        .arg("branch")
+        .arg("-d")
+        .arg(name.trim())
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+
+    println!("{}", String::from(String::from_utf8_lossy(&output.stdout)));
+}
+
+fn get_list_of_branches() -> Vec<String> {
+    let output = Command::new("git")
+        .arg("branch")
+        .arg("--no-merged")
+        .arg("master")
         .output()
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 
     let output: String = String::from(String::from_utf8_lossy(&output.stdout));
-    if output.contains("github.com") {
-        let mut v: Vec<&str> = output.split("/").collect();
-        v = v[v.len() - 1].split(".").collect();
-        return String::from(v[0]);
+
+    let lines: Vec<&str> = output.split("\n").collect();
+
+    let mut res: Vec<String> = Vec::new();
+
+    for i in &lines {
+        let s: String = String::from(*i);
+        res.push(s);
     }
 
-    let s = String::new();
-    s
+    res
 }
