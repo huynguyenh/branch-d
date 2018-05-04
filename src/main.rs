099e310c903ad32b7eb02ef0e3f24c3bec46bb2b
huynguyenh <hoanghuy2908@gmail.com>
2018-05-05 02:27:29 +0700

    add fetch options

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,75 @@
 extern crate clap;
 
-use clap::App;
+use clap::{App, Arg};
 
 use std::process::Command;
 
 fn main() {
-    let _matches = App::new("bd")
-        .version("0.1")
+    const DEFAULT_BRANCH: &str = "develop";
+    let matches = App::new("branch-d")
+        .version("0.2")
         .author("huynguyen <hoanghuy2908@gmail.com>")
         .about("Do some stupid stuff noone care to do")
+        .arg(
+            Arg::with_name("branch")
+                .short("b")
+                .help("Delete branches which have been merged into input branch")
+                .required(false)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("fetch")
+                .short("f")
+                .help("fetch all branches before delete")
+                .takes_value(false)
+                .required(false),
+        )
         .get_matches();
 
-    let list_of_branches = get_list_of_branches();
+    let br: String = String::from(matches.value_of("branch").unwrap_or(DEFAULT_BRANCH));
+    let fetch: bool = matches.is_present("fetch");
+
+    let list_of_branches = get_list_of_branches(&br, fetch);
 
     if list_of_branches.len() > 1 {
         for i in list_of_branches {
             if i.len() > 0 {
-                delete_branch(&i)
+                if i != "master" || i != "develop" {
+                    delete_branch(&i)
+                }
             }
         }
     }
 }
 
 fn delete_branch(name: &String) {
-    let output = Command::new("git")
-        .arg("branch")
-        .arg("-d")
-        .arg(name.trim())
+    println!("start delete local branch {}", name);
+    let mut output = Command::new("git")
+        .args(&["branch", "-d", name.trim()])
+        .output()
+        .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+
+    println!("{}", String::from(String::from_utf8_lossy(&output.stdout)));
+
+    println!("start delete remote branch {}", name);
+    output = Command::new("git")
+        .args(&["push", "origin", "--delete", name.trim()])
         .output()
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 
     println!("{}", String::from(String::from_utf8_lossy(&output.stdout)));
 }
 
-fn get_list_of_branches() -> Vec<String> {
+fn get_list_of_branches(br_name: &String, is_fetch: bool) -> Vec<String> {
+    if is_fetch {
+        println!("fetching remote branches");
+        let _ = Command::new("git")
+            .args(&["fetch", "origin"])
+            .output()
+            .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
+    }
     let output = Command::new("git")
-        .arg("branch")
-        .arg("--merged")
-        .arg("develop")
+        .args(&["branch", "--merged", br_name.trim()])
         .output()
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 
